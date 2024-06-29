1fa50993389e3979feacf89588623e103974f27b
hoanvipro <130170857+hoanvipro@users.noreply.github.com>
2024-06-29 08:54:11 +0700

    Update APB_UART_top.v

--- DUT/APB_UART_top.v
+++ DUT/APB_UART_top.v
@@ -2,8 +2,6 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
-// 
-// Create Date:    14:57:09 05/30/2024 
 // Design Name: 
 // Module Name:    APB_UART_top 
 // Project Name: 
